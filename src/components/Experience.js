f7f58871432820a52a2cbb071edb1b9e024382e4
Jessica Joseph <jessjeanjo@gmail.com>
2019-01-25 07:01:39 -0500

    add some more experience photos

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -4,22 +4,33 @@ import ExperienceCard from './ExperienceCard';
 import jpg0 from '../images/0.JPG';
 import jpg1 from '../images/1.JPG';
 import jpg2 from '../images/2.JPG';
+import png0 from '../images/3.PNG';
+import png1 from '../images/4.PNG';
+import jpg3 from '../images/5.JPG';
+import jpg4 from '../images/6.JPG';
+import jpg5 from '../images/7.JPG';
+import jpg6 from '../images/8.JPG';
+import jpg7 from '../images/9.JPG';
 
 class Experience extends Component {
 	constructor(props) {
 		super(props);
+		
+		this.state = { spans: 0 };
 	}
 
-	experiences = [jpg0, jpg1, jpg2];	
-	
-	createExperienceCard = (experienceUrl) => {
-		return <img src={experienceUrl} />;
+	experiences = [jpg0, jpg1, jpg2, png0, png1, jpg3, jpg4, jpg5, jpg6, jpg7];	
+
+	createExperienceCard = (experience) => {
+		return ( <ExperienceCard image={experience} />);
 	}
 
 	render() {
 		return(
-			<div className='experience--container'>
-				{this.experiences.map(this.createExperienceCard)}
+			<div className='experience--overview'>
+				<div className='experiences--container'>
+					{this.experiences.map(this.createExperienceCard)}
+				</div>
 			</div>
 		);
 	}
--- src/components/ExperienceCard.js
+++ src/components/ExperienceCard.js
@@ -3,12 +3,27 @@ import React, { Component } from 'react';
 class ExperienceCard extends Component {
 	constructor(props) {
 		super(props);
+		
+		this.imageRef = React.createRef();
+		this.state = { spans: 0};
 	}
+
+        setSpans = () => {
+                console.log(this.imageRef.current.clientHeight);
+
+                const height = this.imageRef.current.clientHeight;
+                const spans = Math.ceil(height / 10);
+                this.setState({ spans });
+        }
+
+        componentDidMount() {
+		this.imageRef.current.addEventListener('load', this.setSpans);
+        }	
 
 	render() {
 		return(
-			<div className='experience--container'>
-				<img src={this.props.url} />
+			<div style={{gridRowEnd: `span ${this.state.spans}`}}>
+				<img ref={this.imageRef} src={this.props.image}/>
 			</div>
 		);
 	}
